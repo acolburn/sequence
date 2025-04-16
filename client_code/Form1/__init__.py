1eb7947eb023692c90d1c2e9b28f88f9cf771ed1
acolburn3@duck.com <acolburn3@duck.com>
2025-04-16 01:35:25 +0000

    Edited chipGreen_border.png, sequence_board.png, chipBlue_border.png and Form1; Deleted cardHeartsA.png

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,16 +10,16 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.IMAGE_WIDTH = 80
+    self.IMAGE_WIDTH = 80*0.8
     # self.IMAGE_HEIGHT = 107
-    self.IMAGE_HEIGHT = 80
+    self.IMAGE_HEIGHT = 80*0.8
     # self.GRID_COLS = 10
     # self.GRID_ROWS = 10
-    # self.images = {
-      # 'AH': URLMedia('_/theme/cardHeartsA.png')
-    # }
+    self.images = {
+      'board': URLMedia('_/theme/sequence_board.png')
+    }
 
-    # self.model = []
+    self.model = [{'url':'_/theme/sequence_board.png', 'x':0, 'y':0}]
     # for row in range(self.GRID_ROWS):
       # for col in range(self.GRID_COLS):
         # self.model.append({'type':'AH',
@@ -39,17 +39,18 @@ class Form1(Form1Template):
 
 
   def canvas_1_reset(self, **event_args):
-    self.canvas_1.draw_image(URLMedia('_/theme/sequence_board.png'),0,0)
     # Adjust these coordinates if you want the drawing area to not be centered
     # self.canvas_offset = (self.canvas_1.get_width() - self.canvas_size)/2
     # self.canvas_1.translate(self.canvas_offset, 0)
 
-    # for shape in self.model:
-      # if shape['type'] in self.images:
-        # self.canvas_1.draw_image(self.images[shape['type']], shape['x'], shape['y'])
+    for item in self.model:
+        self.canvas_1.draw_image(URLMedia(item['url']), item['x'], item['y'])
 
   def draw_flag(self, col, row):
-    self.canvas_1.draw_image(URLMedia('_/theme/flag.png'), col*self.IMAGE_WIDTH+7, row*self.IMAGE_HEIGHT+7)
+    flag = {'url':'_/theme/flag.png', 'x': col*self.IMAGE_WIDTH+7, 'y': row*self.IMAGE_HEIGHT+7}
+    self.model.append(flag) if flag not in self.model else None #preventing duplicate entries, which could result in flags getting drawn over and over
+    # self.canvas_1.draw_image(URLMedia('_/theme/flag.png'), col*self.IMAGE_WIDTH+7, row*self.IMAGE_HEIGHT+7)
+    self.canvas_1_reset()
 
   def draw_flag_by_card(self, card):
     # card is a string representation of an individual playing card
@@ -79,9 +80,11 @@ class Form1(Form1Template):
     col = x//self.IMAGE_WIDTH
     print(f"col = {col}, row = {row}")
     # Draw green chip where user clicks
-    self.canvas_1.draw_image(URLMedia('_/theme/chipGreen_border.png'),x-30,y-30)
+    # self.canvas_1.draw_image(URLMedia('_/theme/chipGreen_border.png'),x-30,y-30)
+    chip = {'url':'_/theme/chipGreen_border.png', 'x':x-24, 'y':y-24}
+    self.model.append(chip) if chip not in self.model else None
     # Draw flags for A, K, Q, and 10 of Hearts
-    self.draw_flag_by_hand(['AH','KH','QH','10H'])
+    self.draw_flag_by_hand(['AH','KH','QH','10H']) #this method ultimately calls canvas1_reset()
 
     
    
